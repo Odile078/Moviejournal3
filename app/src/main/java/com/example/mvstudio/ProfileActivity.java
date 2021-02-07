f74c14f4f3f312a189c2d6ca5b50016f7fb4901a
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-02-08 00:27:54 +0200

    connecting the profile to the gallery

--- app/src/main/java/com/example/mvstudio/ProfileActivity.java
+++ app/src/main/java/com/example/mvstudio/ProfileActivity.java
@@ -2,7 +2,9 @@ package com.example.mvstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -59,4 +61,15 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                profileImage.setImageURI(imageUri);
+            }
+        }
+    }
+
 }
